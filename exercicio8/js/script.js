40a257220405b33d522e4866cdeac66825b6736e
nicolas433 <ngrisoste@gmail.com>
2019-12-13 11:01:42 -0300

    Oitava questão

--- exercicio8/js/script.js
+++ exercicio8/js/script.js
@@ -1,52 +1,8 @@
-const entrada = prompt("Digite seu nome.");
-let valido = true;
+const nome = prompt("Digite seu nome.");
+const pos  = parseInt(prompt("Digite um número."));
 
-
-function validate1(entrada){
-    const aux = entrada.split(" ");
-    if(aux[0].length<4){
-        alert("Primeiro nome precisa conter mais que 4 caracteres.");
-        valido = false;
-    }
-}
-
-function validate2(entrada){
-    const aux = entrada.split(" ");
-    aux.forEach(element => {
-        if(element == "do" || element == "da" || element == "de"){
-
-        }else if(element[0]!=element[0].toUpperCase()){
-            alert(`Primeiro caractere da palavra " ${element} " precisa ser maiusculo.`);
-            valido = false;
-        }
-    });
-}
-
-function validate3(entrada){
-    const aux = entrada.split(" ");
-    if(!aux[1]){
-        alert(`Deve existir pelo menos um sobrenome.`);
-        valido = false;
-    }
-}
-
-function validate4(entrada){
-    for(let i = 0; i < entrada.length; i++) {
-        if(parseFloat(entrada[i]) != NaN){
-            alert(`Não pode existir número em nenuma parte do nome.`);
-            valido = false;
-            break;
-        }
-    }
-}
-
-validate1(entrada);
-validate2(entrada);
-validate3(entrada);
-validate4(entrada);
-
-if(valido === true){
-    alert("Nome válido.")
+if(pos>nome.length || pos<0){
+    alert("erro");
 }else{
-    alert("Nome invalido.")
+    alert(nome.charAt(pos));
 }
