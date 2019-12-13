ca7282a4f766c22afe16571cdc787ae870cc42b4
nicolas433 <ngrisoste@gmail.com>
2019-12-13 11:19:17 -0300

    Nona questão

---
+++ exercicio9/js/script.js
@@ -0,0 +1,7 @@
+const dataAtual = new Date();
+const horaAtual = dataAtual.getHours();
+
+(horaAtual<=5)? alert("Boa madrugada") : 
+(horaAtual<=12)? alert("Bom dia") : 
+(horaAtual<=18)? alert("Boa tarde") : 
+alert("Boa noite");
