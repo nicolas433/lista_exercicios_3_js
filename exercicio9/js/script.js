8e4ff101a929d5ee7658e9ccc3b9323ab5310617
Nicolas Grisoste <48197098+nicolas433@users.noreply.github.com>
2019-12-13 20:58:10 -0300

    Update script.js

--- exercicio9/js/script.js
+++ exercicio9/js/script.js
@@ -1,7 +1,9 @@
-const dataAtual = new Date();
-const horaAtual = dataAtual.getHours();
+(function(){
+    const dataAtual = new Date();
+    const horaAtual = dataAtual.getHours();
 
-(horaAtual<=5)? alert("Boa madrugada") : 
-(horaAtual<=12)? alert("Bom dia") : 
-(horaAtual<=18)? alert("Boa tarde") : 
-alert("Boa noite");
+    (horaAtual<=5)? alert("Boa madrugada") : 
+    (horaAtual<=12)? alert("Bom dia") : 
+    (horaAtual<=18)? alert("Boa tarde") : 
+    alert("Boa noite");
+})();
